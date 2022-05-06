5b993d3770752ece05b279838c9ffbf12c50bf63
Emil <emildahl05@gmail.com>
2022-05-06 16:32:17 +0200

    ut1

--- src/Member.java
+++ src/Member.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+
 public class Member extends user{
 
-    int typeOfUser;
+    ArrayList<Member> memberList = new ArrayList<>();
 
 
-    public Member(String peter, int id, boolean admin) {
-        super(peter, id, admin);
+    public Member(String surName, String lastName, boolean admin, String pid, int typeOfUser) {
+        super(surName, lastName, admin, pid, typeOfUser);
     }
+
+    public ArrayList<Member> getMemberList() {
+        return memberList;
+    }
+
+    public void addMember(Member member) {
+        memberList.add(member);
+    }
+
+
 }
